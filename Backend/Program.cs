4b10a827cbfa4db2a5fa74ea7f7ee960d58895e5
lucia4585 <lucilencina6@gmail.com>
2025-12-13 17:24:00 -0300

    Deshabilitar configuración y arranque en Program.cs
    Se han comentado todas las líneas de configuración y arranque de la aplicación en Program.cs, incluyendo servicios, DbContext, CORS, Swagger y el pipeline HTTP. El archivo queda sin ejecución activa ni configuración de servicios, probablemente para tareas de depuración o mantenimiento temporal. No se han realizado cambios en la lógica de la aplicación.

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -58,3 +58,66 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
+
+//using Backend.DataContext;
+//using Microsoft.EntityFrameworkCore;
+//using System.Text.Json.Serialization;
+
+
+//var builder = WebApplication.CreateBuilder(args);
+
+//var configuration = new ConfigurationBuilder()
+//.AddJsonFile("appsettings.json")
+//.Build();
+//string? cadenaConexion = configuration.GetConnectionString("mysqlRemote");
+
+////configuración de inyección de dependencias del DBContextdotnet dev-certs https --trust
+
+//builder.Services.AddDbContext<MusicCoreContext>(
+//    options => options.UseMySql(cadenaConexion,
+//                                ServerVersion.AutoDetect(cadenaConexion),
+//                    options => options.EnableRetryOnFailure(
+//                                        maxRetryCount: 5,
+//                                        maxRetryDelay: System.TimeSpan.FromSeconds(30),
+//                                       errorNumbersToAdd: null)
+//                                ));
+
+//// Add services to the container.
+
+//builder.Services.AddControllers()
+//    .AddJsonOptions(x =>
+//        x.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles);
+//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+//// Configurar una política de CORS
+//builder.Services.AddCors(options =>
+//{
+//    options.AddPolicy("AllowSpecificOrigins",
+//        builder => builder
+//            .WithOrigins("https://localhost:8000", "https://webmusicore.azurewebsites.net")
+//            .AllowAnyHeader()
+//            .AllowAnyMethod());
+//});
+
+//var app = builder.Build();
+
+//app.UseCors("AllowSpecificOrigins");
+
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+//app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//app.Run();
